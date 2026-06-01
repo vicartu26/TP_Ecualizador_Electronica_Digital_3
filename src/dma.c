d310de9db779b8c0cfac1400a7ff9cccd34a8be1
Ange Moises <angelica.moises@mi.unc.edu.ar>
2026-06-01 17:09:50 -0300

    clear flags dma irq handler

--- src/dma.c
+++ src/dma.c
@@ -131,6 +131,6 @@ void DMA_IRQHandler(void)
     {
         FLAGBUFFERB = 1U;
     }
-
-    direccion ^= 1U;   /* Alterna entre 0 y 1 con XOR (mas claro que if/wrap) */
+    direccion ^= 1U;   // Alterna entre 0 y 1 con XOR (mas claro que if/wrap)
+    GPDMA_ClearIntPending(GPDMA_CLR_INTTC, DMA_CH_ADC_CAPTURE); // limpio bandera
 }
