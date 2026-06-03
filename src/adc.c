f922e783c670969f22cebbbe3a05d4437a5b25e2
Ange Moises <angelica.moises@mi.unc.edu.ar>
2026-06-03 11:52:44 -0300

    Cambio ADC a modo BURST

--- src/adc.c
+++ src/adc.c
@@ -1,11 +1,11 @@
 #include "lpc17xx_adc.h"
 
+#define ADC_RATE 16000U  // Frecuencia de muestreo en Hz (max 4.5MHz)
+
 void ConfADC(void){
-    ADC_Init(1000000);
+    ADC_Init(ADC_RATE);                 // rate in Hz (set to 16000)
     ADC_PowerUp();
     ADC_PinConfig(ADC_CHANNEL_0);       // P0.23 -> AD0.0 
-    ADC_BurstDisable();                 // no burst, we use a trigger
-    ADC_StartCmd(ADC_START_ON_MAT01);   // MAT0.1 triggers conversion 15,87kHz
-    ADC_EdgeStartConfig(ADC_START_ON_RISING);
+    ADC_BurstEnable();                  // Burst mode: continous conversions
     ADC_ChannelEnable(ADC_CHANNEL_0);
 }
