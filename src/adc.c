727d75a0e8ff31f079d660a205718288b6dc1bf7
Ange Moises <angelica.moises@mi.unc.edu.ar>
2026-06-03 12:24:18 -0300

    ADC frecuencia 44k y DAC timeout correspondiente

--- src/adc.c
+++ src/adc.c
@@ -1,11 +1,9 @@
-#include "lpc17xx_adc.h"
-
-#define ADC_RATE 16000U  // Frecuencia de muestreo en Hz (max 4.5MHz)
+#include "adc.h"
 
 void ConfADC(void){
-    ADC_Init(ADC_RATE);                 // rate in Hz (set to 16000)
+    ADC_Init(ADC_FS_HZ);                // rate in Hz (set to 64000)
     ADC_PowerUp();
-    ADC_PinConfig(ADC_CHANNEL_0);       // P0.23 -> AD0.0 
+    ADC_PinConfig(ADC_AUDIO_CHANNEL);   // P0.23 -> AD0.0 
     ADC_BurstEnable();                  // Burst mode: continous conversions
-    ADC_ChannelEnable(ADC_CHANNEL_0);
+    ADC_ChannelEnable(ADC_AUDIO_CHANNEL);
 }
